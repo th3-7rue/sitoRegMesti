6937e9745650b1c374ec19c2d223424eae13e574
th3-7rue <66943187+th3-7rue@users.noreply.github.com>
2022-11-12 10:54:34 +0000

    back

---
+++ main.js
@@ -0,0 +1,13 @@
+let nome = document.getElementById("nome")
+let cognome = document.getElementById("cognome")
+let sesso = document.getElementById("sesso")
+let dataNascita = document.getElementById("dataNascita")
+let email = document.getElementById("email")
+let password = document.getElementById("password")
+let btnRegistrati = document.getElementById("btnRegistrati")
+let btnConferma = document.getElementById("btnConferma")
+let myAlert = document.getElementById("my-modal")
+
+btnConferma.onclick = function () {
+    
+}
