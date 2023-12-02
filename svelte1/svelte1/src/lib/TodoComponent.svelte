0eac3573914ea1c14e180eaba8fc41764a87d341
Riccardo Rasori <66943187+th3-7rue@users.noreply.github.com>
2023-12-02 10:36:15 +0000

    f

--- svelte1/svelte1/src/lib/TodoComponent.svelte
+++ svelte1/svelte1/src/lib/TodoComponent.svelte
@@ -1,11 +1,19 @@
 <script>
+  import { todos } from "./store";
+
   export let todo;
   export let pos;
+  function deleteTodo() {
+    todos.update(old=>{
+      old.splice(pos,1)
+      return old
+    })
+  }
 </script>
 
 <div>
   {todo.title} 
- <button class="btn btn-square">
+ <button class="btn btn-square" on:click={deleteTodo}>
   <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
 </button>
 </div>
